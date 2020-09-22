971dc0f48ca2cd1dde1921d2fbce269f1c6316bd
shin nagai <shnnagai@gmail.com>
2020-09-23 00:08:42 +0900

    clean up src

--- django-apache/src/data_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import FavoriteCommand
-
-
-class SaveFavoriteCommandSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FavoriteCommand
-        fields = ('name', 'company', 'sql', 'previous_period')
